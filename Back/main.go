4c1b13992ef444e0cdca6487bfff217327942100
soosungp33 <soosungp33@gmail.com>
2021-05-12 17:07:44 +0900

    first commit

---
+++ Back/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HomePage(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "홈페이지 입니다.",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", HomePage)
+	r.Run(":8080")
+}
